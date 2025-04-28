1013b251a8f8f97d34c472d96be8c47f977c4dec
krishna sai <krishsai0303@gmail.com>
2025-04-28 10:59:05 +0530

    added title to the feedback page

--- routes/feedback.tsx
+++ routes/feedback.tsx
@@ -1,8 +1,13 @@
 import FeedbackForm from "../islands/FeebackForm.tsx";
+import { Head } from "$fresh/runtime.ts";
 
 export default function FeedbackPage() {
  return (
   <div class="min-h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950">
+    <Head>
+      <title>Feedback</title>
+      <meta name="description" content="Feedback page for Zapreach" />
+    </Head>
     <FeedbackForm />
   </div>
   ); 
